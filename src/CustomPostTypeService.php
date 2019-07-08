12b2689fd3c012ed0d4a70b6acbe408f3288add6
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2019-07-08 18:33:32 +0200

    new methods to create and save a custom field form based on meta definitions

--- src/CustomPostTypeService.php
+++ src/CustomPostTypeService.php
@@ -2,6 +2,7 @@
 
 namespace WonderWp\Component\CPT;
 
+use WonderWp\Component\DependencyInjection\Container;
 use WonderWp\Component\PluginSkeleton\AbstractManager;
 use WonderWp\Component\Repository\PostRepository;
 use WonderWp\Component\Service\AbstractService;
@@ -50,9 +51,7 @@ class CustomPostTypeService extends AbstractService
      */
     public function register()
     {
-        $this->customPostType->register();
-
-        return $this;
+        return $this->customPostType->register();
     }
 
     /**
@@ -96,14 +95,85 @@ class CustomPostTypeService extends AbstractService
 
     /**
      * Call this method in the cpt hok service to fix the cpt dashboard url
+     *
      * @param string $computedUrl , the original url
      *
      * @return string , the new url
      */
     public function fixCptDashboardUrl($computedUrl)
     {
-        $computedUrl = admin_url('edit.php?post_type='.$this->customPostType->getName());
+        $computedUrl = admin_url('edit.php?post_type=' . $this->customPostType->getName());
 
         return $computedUrl;
+    }
+
+    /**
+     * Create a meta box with a form to administer post metas based on meta definitions
+     */
+    public function createMetasForm()
+    {
+        $metasDefinition = $this->customPostType->getMetaDefinitions();
+        if (!empty($metasDefinition)) {
+            add_meta_box('cpts_meta_box',
+                __('cpts.metabox.title'),
+                [$this, 'displayCptsMetaBox'],
+                $this->customPostType->getName(), 'normal', 'high'
+            );
+        }
+    }
+
+    /**
+     * The automatic meta form
+     *
+     * @param \WP_Post $post
+     */
+    public function displayCptsMetaBox(\WP_Post $post)
+    {
+        $metasDefinition = $this->customPostType->getMetaDefinitions();
+        if (!empty($metasDefinition)) {
+            $container = Container::getInstance();
+            /** @var Form $form */
+            $form        = $container['wwp.form.form'];
+            $metasValues = !empty($post->ID) ? get_post_meta($post->ID) : [];
+
+            foreach ($metasDefinition as $metaKey => $metaDef) {
+                $inputType       = $metaDef[0];
+                $savedMetaValue  = !empty($metasValues[$metaKey]) ? reset($metasValues[$metaKey]) : (!empty($metaDef[1]) ? $metaDef[1] : null);
+                $displayRules    = !empty($metaDef[2]) ? $metaDef[2] : [];
+                $validationRules = !empty($metaDef[3]) ? $metaDef[3] : [];
+                $f               = new $inputType($metaKey, $savedMetaValue, $displayRules, $validationRules);
+                $form->addField($f);
+            }
+
+            echo $form->renderView([
+                'formStart' => ['showFormTag' => false],
+                'formEnd'   => ['showFormTag' => false, 'showSubmit' => false],
+            ]);
+        }
+    }
+
+    /**
+     * Save meta box form submitted data into post metas
+     *
+     * @param          $post_id
+     * @param \WP_Post $post
+     */
+    public function saveMetasForm($post_id, \WP_Post $post)
+    {
+        $metasDefinition = $this->customPostType->getMetaDefinitions();
+
+        // Check post type
+        if ($post->post_type == $this->customPostType->getName()) {
+            // Store data in post meta table if present in post data
+
+            $metasDefinition = $this->customPostType->getMetaDefinitions();
+            if (!empty($metasDefinition)) {
+                foreach ($metasDefinition as $metaKey => $metaDef) {
+                    if (isset($_POST[$metaKey])) {
+                        update_post_meta($post_id, $metaKey, $_POST[$metaKey]);
+                    }
+                }
+            }
+        }
     }
 }
